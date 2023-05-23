2b4a4903c4276cfb64ec39ef2a8289c8d27d85d0
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-05-23 03:24:15 +0300

    use ESM and add length check

--- lab2/task2.ts
+++ lab2/task2.ts
@@ -1,4 +1,8 @@
 const isAnagram = (str1: string, str2: string): boolean => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  
   return str1.toLowerCase().trim().split('').reverse().join('') === str2.toLowerCase().trim();
 }
 
